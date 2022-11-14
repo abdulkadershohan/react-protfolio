3acc88433343dfbd0480212967266fd9d609686d
MD. Abdul Kader <abdulkadirshohan@gmail.com>
2022-11-14 20:19:50 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
+import './font.css';
+import logo from './logo.svg';
 
 function App() {
   return (
@@ -7,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> Elias is a web designer and front-end developer
         </p>
         <a
           className="App-link"
