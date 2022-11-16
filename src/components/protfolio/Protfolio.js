b7e5940fd961863d77979662516ee0cd38bb2d71
MD. Abdul Kader <abdulkadirshohan@gmail.com>
2022-11-16 12:00:48 +0600

    home done

--- src/components/protfolio/Protfolio.js
+++ src/components/protfolio/Protfolio.js
@@ -1,41 +1,46 @@
 import { Box, Stack } from "@mui/system";
 import React from "react";
 import { Link } from 'react-router-dom';
-import chertnodes from '../../assets/images/projects/chertnodes.jpg';
-import khanswers from '../../assets/images/projects/khanswers.jpg';
-import madhost from '../../assets/images/projects/madhost.jpg';
+
+import brdchart from '../../assets/images/projects/brdchart.png';
+import covid from '../../assets/images/projects/covid-19.png';
+import upcopo2 from '../../assets/images/projects/upocopo-2.png';
 import { CButton, CTypography } from "../../utility";
 const cardData = [
     {
         id: 1,
-        title: 'Card 1',
-        skils: ['HTML', 'CSS', 'JS'],
-        subtitle: 'Card 1 subtitle',
-        description: 'Card 1 description',
-        cardImage: chertnodes,
-        liveUrl: 'https://chertnodes.com/',
-        figma: "https://chertnodes.com/",
+        title: 'Upcopo',
+        skils: ["React Native", "Redux", "Redux Toolkit", "Native Base", "expo", "formik", "yup", "Google Map"],
+        description: `NFTs app for Buy Virtual Real Estate.`,
+        cardImage: upcopo2,
+        liveUrl: '',
+        youtubeUrl: 'https://www.youtube.com/watch?v=k7098_rTLlA&feature=youtu.be',
+        figma: "https://www.figma.com/file/xxFZjSHJXT2yrQwS68DCDm/Upcopo?node-id=412%3A712",
+        githubUrl: ''
     },
     {
         id: 2,
-        title: 'Card 1',
-        skils: ['HTML', 'CSS', 'JS', 'React', 'Node', 'MongoDB', 'Express', 'Next', 'Redux', 'Material UI', 'Tailwind CSS'],
-        subtitle: 'Card 1 subtitle',
-        description: 'Card 1 description',
-        cardImage: khanswers,
-        liveUrl: 'https://chertnodes.com/',
-        figma: "",
+        title: 'Brdcart',
+        skils: ["React Native", "Redux", "Redux Toolkit", "Native Base", "expo", "formik", "yup", "Google Map"],
+        description: 'E-commerce app',
+        cardImage: brdchart,
+        liveUrl: '',
+        figma: "https://www.figma.com/file/HPX2Q9k7QC0jughEcor3Ad/Bai1234?fuid=1020350840147497557",
+        youtubeUrl: '',
+        githubUrl: ''
 
     },
     {
         id: 3,
-        title: 'ProtectX web',
-        skils: ['HTML', 'CSS', 'JS'],
-        subtitle: 'Minecraft servers hosting',
-        description: 'Card 1 description',
-        cardImage: madhost,
+        title: 'Covid-19 Survey',
+        skils: ["Bootstrap", "ReactJS", "EmailJS", "Styled Component",
+            "HTML", "CSS", "JavaScript", "PHP", "MYSQL"],
+        description: 'Helth Care App',
+        cardImage: covid,
         figma: "",
-        liveUrl: 'https://chertnodes.com/',
+        liveUrl: 'https://covid-19-survey-form-2020.netlify.app/',
+        // youtubeUrl: 'https://www.youtube.com/watch?v=0KnPfVyOxeg&t=5s',
+        githubUrl: "https://github.com/abdulkadershohan/COVID-19-SURVEY-FROM"
     },
 ]
 
@@ -113,7 +118,7 @@ const ProjectCard = () => {
                                     {item.title}
                                 </CTypography>
                                 <CTypography
-                                    fontSize={16}
+                                    fontSize={14}
                                     fontWeight={400}
                                     color="#ABB2BF"
                                 >
@@ -123,12 +128,44 @@ const ProjectCard = () => {
                                     display="flex"
                                     gap={2}
                                 >
-                                    <CButton
-                                        btnTitle="Live =>"
-                                    />
+                                    {
+                                        item.liveUrl && <CButton
+                                            btnTitle="Live =>"
+                                            btnLink={item.liveUrl}
+                                            component={'a'}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            href={item.liveUrl}
+                                        />
+                                    }
                                     {
                                         item.figma && <CButton
                                             btnTitle="Figma =>"
+                                            btnLink={item.figma}
+                                            component={'a'}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            href={item.figma}
+                                        />
+                                    }
+                                    {
+                                        item.youtubeUrl && <CButton
+                                            btnTitle="Youtube =>"
+                                            component={'a'}
+                                            href={item.youtubeUrl}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+
+                                        />
+                                    }
+                                    {
+                                        item.githubUrl && <CButton
+                                            btnTitle="Github repo =>"
+                                            component={'a'}
+                                            href={item.githubUrl}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+
                                         />
                                     }
 
