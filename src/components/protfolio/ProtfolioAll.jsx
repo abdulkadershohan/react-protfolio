fb584d02cb37650f05c911062db0dea675ea165c
MD. Abdul Kader <abdulkadirshohan@gmail.com>
2022-11-17 15:48:14 +0600

    scroll bug

--- src/components/protfolio/ProtfolioAll.jsx
+++ src/components/protfolio/ProtfolioAll.jsx
@@ -353,10 +353,6 @@ const ProjectCard = ({ data }) => {
 export default function Protfolio() {
     const navigate = useNavigate();
     const { mobileAppTitle, mobileApp, webAppTitle, webApp } = ProtfolioData;
-    // React.useEffect(() => {
-    //     window.scrollTo(0, 0);
-    // }, [navigate])
-
     const HeaderWeb = () => {
         return (
             <Stack
